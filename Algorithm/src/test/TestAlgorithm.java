731bf795483bde847a07738773e8828b716e578d
choisanghyun <itchoi0429@gmail.com>
2019-05-08 20:44:41 +0900

    단순 선택 정렬 및 단순 삽입 정렬...

--- Algorithm/src/test/TestAlgorithm.java
+++ Algorithm/src/test/TestAlgorithm.java
@@ -1,46 +1,31 @@
 package test;
 
-import java.util.Scanner;
-
 public class TestAlgorithm {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("버블 정렬(버전 1)");
-		System.out.println("요솟수 : ");
-		int nx = sc.nextInt();
-		int[] x = new int[nx];
+		int nx = 7;
+		int[] x = new int[] {22,5,11,32,120,68,70};
+	
+		binInsertionSort(x, nx);
 		
-		for (int i = 0; i < nx; i++) {
-			System.out.println("x[" + i + "]: ");
-			x[i] = sc.nextInt();
+		for (int test : x) {
+			System.out.println("순서: " + test);
 		}
 		
-		bubbleSort(x, nx);
-		
-		System.out.println("오름차순으로 정렬했습니다.");
-		
-		for (int i = 0; i < nx; i++) {
-			System.out.println("x[" + i + "]= " + x[i]);
-		}
 	}
 	
-	// a[idx1]와 a[idx2]의 값을 바꾼다.
-	static void swap(int[] a, int idx1, int idx2) {
-		int t = a[idx1];
-		a[idx1] = a[idx2];
-		a[idx2] = t;
-	}
-	
-	// 버블 정렬
-	static void bubbleSort(int[] a, int n) {
-		for (int i = 0; i < n - 1; i++) {
-			for (int j = n - 1; j > i; j--) {
-				if (a[j - 1] > a[j]) {
-					swap(a, j - 1, j);
-				}
-			}
+	static void binInsertionSort(int[] a, int n) {
+		for (int i = 1; i < n; i++) {
+			int key = a[i];
+			int si = 0;
+			int li = n - 1;
+			int mi;
+			int ci;
+			
+			
+			
 		}
+		
 	}
 	
 }
