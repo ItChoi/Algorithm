a54b9b68c6e84d0cc3f2d4eb11e9e7cd49eae7c5
choisanghyun <itchoi0429@gmail.com>
2019-05-10 19:28:55 +0900

    잔디밭을 만들자... 셸 정렬 공부중...

--- Algorithm/src/test/TestAlgorithm.java
+++ Algorithm/src/test/TestAlgorithm.java
@@ -3,26 +3,20 @@ package test;
 public class TestAlgorithm {
 
 	public static void main(String[] args) {
-		int nx = 7;
-		int[] x = new int[] {22,5,11,32,120,68,70};
-	
-		binInsertionSort(x, nx);
+		int nx = 8;
+		int[] x = new int[] {8,1,4,2,7,6,3,5};
+		
+		shellSort(x, nx);
 		
 		for (int test : x) {
-			System.out.println("순서: " + test);
+			System.out.println("정렬: " + test);
 		}
-		
 	}
 	
-	static void binInsertionSort(int[] a, int n) {
-		for (int i = 1; i < n; i++) {
-			int key = a[i];
-			int si = 0;
-			int li = n - 1;
-			int mi;
-			int ci;
-			
-			
+	static void shellSort(int[] a, int n) {
+		
+		for (int i = n / 2; i > 0; i /= 2) {
+			for (int j = i; )
 			
 		}
 		
