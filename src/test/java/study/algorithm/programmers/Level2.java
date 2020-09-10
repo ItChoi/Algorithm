5d9426404a70d493d1ebe6c0bb4fb57709305235
itchoi <itchoi0429@gmail.com>
2020-09-11 02:36:20 +0900

    algorithm test

---
+++ src/test/java/study/algorithm/programmers/Level2.java
@@ -0,0 +1,193 @@
+package study.algorithm.programmers;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 프로그래머스 알고리즘 문제 2레벨을 풀어보자.
+ */
+public class Level2 {
+
+    // 스택/큐 - 주식가격 start
+    @Test
+    public void 주식가격() {
+        int[] prices = {1,2,3,2,3};
+        for (int price : 주식가격_함수(prices)) {
+            System.out.println("result: " + price);
+        }
+    }
+
+    private int[] 주식가격_함수(int[] prices) {
+        int[] answer = new int [prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            int count = 0;
+
+            for (int j = i; j < prices.length; j++) {
+                if (i == j) continue;
+                count++;
+                if (prices[i] > prices[j]) {
+                    break;
+                }
+            }
+            answer[i] = count;
+        }
+
+        return answer;
+    }
+    // 스택/큐 - 주식가격 end
+
+
+    // 스택/큐 - 프린터 start
+    @Test
+    public void 프린터() {
+        int[] properties = {2, 1, 3, 2};
+        int location = 2;
+
+        /*int[] properties = {1, 1, 9, 1, 1, 1};
+        int location = 0;*/
+        System.out.println("result: " + 프린터_함수(properties, location));
+    }
+
+    private int 프린터_함수(int[] priorities, int location) {
+        int answer = location;
+
+        List<Integer> list = new LinkedList<>();
+        for (int priority : priorities) {
+            list.add(priority);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) < list.get(j)) {
+                    j = i;
+                    list.add(list.remove(i));
+                    if (answer > i) {
+                        answer--;
+                    } else if (answer == i) {
+                        answer = list.size() - 1;
+                    }
+                }
+            }
+        }
+
+        return answer + 1;
+    }
+    // 스택/큐 - 프린터 end
+
+
+    // 스택/큐 - 기능개발 start
+    @Test
+    public void 기능개발() {
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+
+        for (int i : 기능개발_함수(progresses, speeds)) {
+            System.out.println("result: " + i);
+        }
+    }
+
+    private int[] 기능개발_함수(int[] progresses, int[] speeds) {
+        int point = 0;
+        int size = progresses.length;
+
+        List<Integer> list = new ArrayList<>();
+
+        while (point != size) {
+            for (int i = point; i < size; i++) {
+                if (progresses[i] >= 100) {
+                    continue;
+                }
+                progresses[i] += speeds[i];
+            }
+
+            if (progresses[point] >= 100) {
+                int count = 1;
+                point++;
+                for (int i = point; i < size; i++) {
+                    if (progresses[i] < 100) {
+                        break;
+                    }
+                    point++;
+                    count++;
+                }
+
+                list.add(count);
+            }
+
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    // 스택/큐 - 기능개발 end
+
+
+    // Summer/Winter Coding(~2018) - 스킬트리 start
+    @Test
+    public void 스킬트리() {
+        String skill = "CBD";
+        String[] skill_trees = {"BACDE", "CBADF", "AECB", "BDA"};
+//        String[] skill_trees = {"BACDE", "CBADF", "AECB", "BDA", "ZXC"};
+//        String[] skill_trees = {"BDA"};
+//        String[] skill_trees = {"BACDE", "CBADF", "AECB", "BDA", "AQWER"};
+//        String[] skill_trees = {"CXF", "ASF", "BDF", "CEFD"};
+//        String[] skill_trees = {"CEFD"};
+
+        System.out.println("result: " + 스킬트리_함수(skill, skill_trees));
+    }
+
+    private int 스킬트리_함수(String skill, String[] skill_trees) {
+        int answer = 0;
+
+        String firstS = String.valueOf(skill.charAt(0));
+        for (int i = 0; i < skill_trees.length; i++) {
+            boolean success = true;
+            int checkIndex = -1;
+            boolean firstCheck = false;
+
+            if (skill_trees[i].contains(firstS)) {
+                firstCheck = true;
+            }
+
+            for (int j = 0; j < skill.length(); j++) {
+                String s = String.valueOf(skill.charAt(j));
+
+                if (skill_trees[i].contains(s)) {
+                    if (!firstCheck) {
+                        success = false;
+                        break;
+                    }
+
+                    int indexOf = skill_trees[i].indexOf(s);
+                    if (checkIndex > indexOf) {
+                        success = false;
+                        break;
+                    }
+                    int preNum = checkIndex == -1 ? -1 : skill.indexOf(skill_trees[i].charAt(checkIndex));
+                    int nextNum = skill.indexOf(skill_trees[i].charAt(indexOf));
+
+                    if (preNum + 1 != nextNum) {
+                        success = false;
+                        break;
+                    }
+
+                    checkIndex = indexOf;
+                }
+            }
+
+            if (success) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+    // Summer/Winter Coding(~2018) - 스킬트리 end
+
+
+
+}
