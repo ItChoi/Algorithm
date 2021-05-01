26a671e20fbd948b28e02dc19ba569808f0773db
최상현 <itchoi0429@gmail.com>
2021-05-02 01:20:36 +0900

    asd

--- src/test/java/study/algorithm/inflearn/Main.java
+++ src/test/java/study/algorithm/inflearn/Main.java
@@ -701,13 +701,51 @@ public class Main {
                 }
             }
         }*/
+        return answer;
+    }
+    // 8. 등수구하기 end
 
+    // 9. 격자판 최대합 start
+    @Test
+    void 격자판_최대합() {
+        int n = 5;
+        int[][] arr = {
+            {10, 13, 10, 12, 15},
+            {12, 39, 30, 23, 11},
+            {11, 25, 50, 53, 15},
+            {19, 27, 29, 37, 27},
+            {19, 13, 30, 13, 19}
+        };
+        System.out.println("resutl::: " + 격자판_최대합_함수(n, arr));
+    }
+    private int 격자판_최대합_함수(int n, int[][] arr) {
+        int answer = 0;
 
+        int sum1;
+        int sum2;
+        for (int i = 0; i < n; i++) {
+            sum1 = 0;
+            sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+
+        sum1 = 0;
+        sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][n - i - 1];
+        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
 
         return answer;
     }
-
-    // 8. 등수구하기 end
+    // 9. 격자판 최대합 end
 
     // Array(1, 2차원 배열) end
 }
