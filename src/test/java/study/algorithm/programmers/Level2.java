a443aae0d0505127409d4a361be56718eafcebf6
최상현 <itchoi0429@gmail.com>
2021-06-20 17:06:52 +0900

    땅따먹기

--- src/test/java/study/algorithm/programmers/Level2.java
+++ src/test/java/study/algorithm/programmers/Level2.java
@@ -639,11 +639,29 @@ public class Level2 {
             {6, 6, 6, 4},
             {8, 7, 6, 5}
         };
+
+        /*int[][] land = {
+            {9, 5, 2, 3},
+            {9, 8, 6, 7},
+            {8, 9, 7, 1},
+            {100, 9, 8, 1}
+        };*/
         System.out.println("result: " + 땅따먹기_함수(land));
     }
 
     private int 땅따먹기_함수(int[][] land) {
-        int noTargetIndex = 0;
+        for(int i=1; i<land.length; i++){
+            land[i][0] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][3]);
+            land[i][1] += Math.max(Math.max(land[i-1][0], land[i-1][2]), land[i-1][3]);
+            land[i][2] += Math.max(Math.max(land[i-1][1], land[i-1][0]), land[i-1][3]);
+            land[i][3] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][0]);
+        }
+
+        int[] answer = land[land.length-1];
+        Arrays.sort(answer);
+
+        return answer[answer.length-1];
+        /*int noTargetIndex = 0;
         int max = 0;
         for (int i = 0; i < land[0].length; i++) {
             noTargetIndex = i;
@@ -667,7 +685,7 @@ public class Level2 {
 
         }
 
-        return max;
+        return max;*/
     }
 
     // 연습문제 땅따먹기 END
