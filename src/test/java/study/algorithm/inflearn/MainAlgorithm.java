86f13c48a4b93684a9e51d940f93f8dd813fca22
noah <itchoi0429@gmail.com>
2023-07-17 20:23:27 +0900

    인프런 알고리즘 숫자 뒤집어서 소수 구하기

--- src/test/java/study/algorithm/inflearn/MainAlgorithm.java
+++ src/test/java/study/algorithm/inflearn/MainAlgorithm.java
@@ -2,6 +2,8 @@ package study.algorithm.inflearn;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -494,14 +496,62 @@ public class MainAlgorithm {
             numbers.add(kb.nextInt());
         }
 
-        System.out.print(T.solution(num, numbers));
+        for (Integer integer : T.solution(num, numbers)) {
+            System.out.print(integer + " ");
+        }
     }
 
-    public int solution(int num,
-                        List<Integer> numbers) {
-        int result = 0;
-        // TODO: 풀기
+    public List<Integer> solution(int num,
+                                  List<Integer> numbers) {
+
+        List<Integer> result = new ArrayList<>();
+
+        List<Integer> reversedNumbers = reversedNumbers(numbers);
+        int maxNum = calculateMaxNum(reversedNumbers);
+        boolean[] isSosu = new boolean[maxNum + 1];
+        Arrays.fill(isSosu, true);
+        isSosu[1] = false;
+
+        for (int i = 2; i * i <= maxNum; i++) {
+            for (int j = i * i; j <= maxNum; j += i) {
+                isSosu[j] = false;
+            }
+        }
+
+        for (int i = 0; i < reversedNumbers.size(); i++) {
+            Integer targetNum = reversedNumbers.get(i);
+            if (isSosu[targetNum]) {
+                result.add(targetNum);
+            }
+        }
+
         return result;
+    }
+
+    private List<Integer> reversedNumbers(List<Integer> numbers) {
+        List<Integer> result = new ArrayList<>();
+
+        for (Integer number : numbers) {
+            char[] cArr = String.valueOf(number).toCharArray();
+            int length = cArr.length / 2;
+
+            for (int i = 0; i < length; i++) {
+                int iLength = cArr.length - i - 1;
+                char tempC = cArr[i];
+                cArr[i] = cArr[iLength];
+                cArr[iLength] = tempC;
+            }
+
+            result.add(Integer.parseInt(new String(cArr)));
+        }
+
+        return result;
+    }
+
+    private int calculateMaxNum(Collection<Integer> numbers) {
+        return numbers.stream()
+            .max(Comparator.comparing(Integer::intValue))
+            .orElse(0);
     }
     // 6. 뒤집은 소수 END
 
