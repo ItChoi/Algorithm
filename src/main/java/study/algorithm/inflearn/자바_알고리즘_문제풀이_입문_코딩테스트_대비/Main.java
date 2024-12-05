736066fbc2e842b5c3c10e49dddc20bdebfcc5e0
최상현 <itchoi0429@gmail.com>
2024-12-05 20:31:50 +0900

    인프런 - 자바 알고리즘 문제 풀이 - 좌표 정렬, 이분 검색(정렬) 풀이 - 강사님 풀이 추가

--- src/main/java/study/algorithm/inflearn/자바_알고리즘_문제풀이_입문_코딩테스트_대비/Main.java
+++ src/main/java/study/algorithm/inflearn/자바_알고리즘_문제풀이_입문_코딩테스트_대비/Main.java
@@ -2903,10 +2903,26 @@ class Main {
         return 0;
     }
 
-    private int 이분검색_08(int N,
-                        int K,
+    private int 이분검색_08(int n,
+                        int m,
                         int[] arr) {
-        int answer = 1;
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 0;
+        int rt = n - 1;
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (arr[mid] == m) {
+                answer = mid + 1;
+                break;
+            }
+
+            if (arr[mid] > m) {
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
 
         return answer;
     }
